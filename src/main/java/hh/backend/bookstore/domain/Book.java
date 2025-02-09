b67bfbbb5a0ebdf5a86fbeac787024ab3078f3af
Joona Koskinen <joonakoskinen03@gmail.com>
2025-02-09 11:51:25 +0200

    JPA H2

--- src/main/java/hh/backend/bookstore/domain/Book.java
+++ src/main/java/hh/backend/bookstore/domain/Book.java
@@ -1,6 +1,15 @@
 package hh.backend.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String author;
     private int publicationYear;
@@ -15,6 +24,14 @@ public class Book {
         this.publicationYear = publicationYear;
         this.isbn = isbn;
         this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
