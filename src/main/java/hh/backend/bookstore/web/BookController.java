ff79afb5e0e483b56b6c30465209d96fea40641f
Joona Koskinen <joonakoskinen03@gmail.com>
2025-02-09 12:10:43 +0200

    booklist

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import hh.backend.bookstore.domain.Book;
+import hh.backend.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
@@ -12,5 +13,16 @@ public class BookController {
     public String welcomePage(Model model){
         model.addAttribute("books", new Book());
         return "welcomepage";
+    }
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/booklist")
+    public String showBookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";  
     }
 }
